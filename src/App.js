106bfbb6bb867e8a76ee1f80b5c406ec0c5c15f8
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2024-03-07 01:46:20 +0600

    .

--- src/App.js
+++ src/App.js
@@ -4,22 +4,36 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name="Rubel" nika="Biju"></Person>
+      <Person name="Sagor" nika="Biju"></Person>
+      <Person nika="Biju"></Person>
+      <Person></Person>
+      <h1>New components</h1>
+      <p id="bekar">React is coming</p>
+      <Friend phone="01767211103"></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
     </div>
   );
 }
 
+function Person(props) {
+  // console.log(props);
+  return (
+    <div className="person">
+      <h1>{props.name}</h1>
+      <p>{props.nika}</p>
+    </div>
+  )
+}
+function Friend(props) {
+  return (
+    <div className="container">
+      <h1>Ratan</h1>
+      <p>Phone:{props.phone}</p>
+    </div>
+  )
+}
+
 export default App;
+
